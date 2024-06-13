5bed21f1d0c3cd9e443c8a907e3d2d3049104f8f
Jordan Christensen <jordan.christensen@nelsonirrigation.com>
2024-06-13 15:01:06 -0700

    handle multi-part string requests

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -33,8 +33,7 @@ defmodule Servy.Handler do
   end
 
   def route(%Conv{method: "POST", path: "/bears"} = conv) do
-    params = %{"name" => "Baloo", "type" => "Brown"}
-    %{conv | status: 201, resp_body: "Created a #{params["type"]} bear named #{params["name"]}!"}
+    %{conv | status: 201, resp_body: "Created a #{conv.params["type"]} bear named #{conv.params["name"]}!"}
   end
 
   # name=Baloo&type=Brown
--- lib/servy/parser.ex
+++ lib/servy/parser.ex
@@ -2,7 +2,7 @@ defmodule Servy.Parser do
   alias Servy.Conv
 
   def parse(request) do
-    [top, params_string] = String.split(request, "\n\n")
+    [top, params_string] = String.split(request, "\n\n", parts: 2)
 
     [request_line | header_lines] = String.split(top, "\n")
 

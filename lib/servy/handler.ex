a779fd4cedd997ead8e4fc698374b215db752483
Jordan Christensen <jordan.christensen@nelsonirrigation.com>
2024-06-28 16:44:38 -0700

    resp_content_type

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -67,7 +67,7 @@ defmodule Servy.Handler do
   def format_response(%Conv{} = conv) do
     """
     HTTP/1.1 #{Conv.full_status(conv)}\r
-    Content-Type: text/html\r
+    Content-Type: #{conv.resp_content_type}\r
     Content-Length: #{String.length(conv.resp_body)}\r
     \r
     #{conv.resp_body}

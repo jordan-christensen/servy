8d6bd1a2a92cd03c09e02706068227170fbfc8d6
Jordan Christensen <jordan.christensen@nelsonirrigation.com>
2024-06-28 12:11:00 -0700

    render function

--- lib/servy/bear_controller.ex
+++ lib/servy/bear_controller.ex
@@ -4,26 +4,26 @@ defmodule Servy.BearController do
 
   @templates_path Path.expand("../../templates", __DIR__)
 
+  defp render(conv, template, bindings \\ []) do
+    content = @templates_path
+      |> Path.join(template)
+      |> EEx.eval_file(bindings)
+
+    %{conv | status: 200, resp_body: content}
+  end
+
   def index(conv) do
     bears =
       Wildthings.list_bears()
       |> Enum.sort(&Bear.order_asc_by_name/2)
 
-      content = @templates_path
-      |> Path.join("index.eex")
-      |> EEx.eval_file(bears: bears)
-
-    %{conv | status: 200, resp_body: content}
+    render(conv, "index.eex", bears: bears)
   end
 
   def show(conv, %{"id" => id}) do
     bear = Wildthings.get_bear(id)
 
-    content = @templates_path
-      |> Path.join("show.eex")
-      |> EEx.eval_file(bear: bear)
-
-    %{conv | status: 200, resp_body: content}
+    render(conv, "show.eex", bear: bear)
   end
 
   def create(conv, %{"type" => type, "name" => name}) do

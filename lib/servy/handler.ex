857f2a5c36c6d951645484930089e3a580d25817
Jordan Christensen <jordan.christensen@nelsonirrigation.com>
2024-07-03 11:47:22 -0700

    update server

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -20,6 +20,10 @@ defmodule Servy.Handler do
     |> format_response
   end
 
+  def route(%Conv{method: "GET", path: "/kaboom"} = conv) do
+    raise "Kaboom!"
+  end
+
   def route(%Conv{method: "GET", path: "/hibernate/" <> time} = conv) do
     time |> String.to_integer() |> :timer.sleep()
 

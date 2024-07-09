2f6f9858b1f439448263f0aa72a654f10d079908
jordan-christensen <jkchristensen36@gmail.com>
2024-07-09 14:05:20 -0700

    comment out client code

--- lib/servy/pledge_server.ex
+++ lib/servy/pledge_server.ex
@@ -1,11 +1,13 @@
 defmodule Servy.PledgeServer do
 
+  @name :pledge_server
+
   def start do
     IO.puts "Starting the pledge server..."
     pid = spawn(__MODULE__, :listen_loop, [[]])
+    Process.register(pid, @name)
+    pid
   end
-
-
 
   def listen_loop(state) do
     receive do
@@ -22,16 +24,16 @@ defmodule Servy.PledgeServer do
     end
   end
 
-  def create_pledge(pid, name, amount) do
-    send(pid, {self(), :create_pledge, name, amount})
+  create_pledge(name, amount) do
+    send(@name, {self(), :create_pledge, name, amount})
 
     receive do
       {:response, status} -> status
     end
   end
 
-  def recent_pledges(pid) do
-    send(pid, {self(), :recent_pledges})
+  def recent_pledges() do
+    send(@name, {self(), :recent_pledges})
 
     receive do
       {:response, pledges} -> pledges
@@ -45,14 +47,14 @@ defmodule Servy.PledgeServer do
 end
 
 
-alias Servy.PledgeServer
+# alias Servy.PledgeServer
 
-pid = PledgeServer.start()
+# pid = PledgeServer.start()
 
-IO.inspect PledgeServer.create_pledge(pid, "larry", 10)
-IO.inspect PledgeServer.create_pledge(pid, "moe", 20)
-IO.inspect PledgeServer.create_pledge(pid, "curly", 30)
-IO.inspect PledgeServer.create_pledge(pid, "daisy", 40)
-IO.inspect PledgeServer.create_pledge(pid, "grace", 50)
+# IO.inspect PledgeServer.create_pledge("larry", 10)
+# IO.inspect PledgeServer.create_pledge("moe", 20)
+# IO.inspect PledgeServer.create_pledge("curly", 30)
+# IO.inspect PledgeServer.create_pledge("daisy", 40)
+# IO.inspect PledgeServer.create_pledge("grace", 50)
 
-IO.inspect(PledgeServer.recent_pledges(pid))
+# IO.inspect(PledgeServer.recent_pledges())
